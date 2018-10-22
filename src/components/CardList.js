f8b47a8a106010f849f5105dcec3b6f511e71c82
FernandoZnga <fernando.zunigag@me.com>
2018-10-22 08:33:25 -0600

    update

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,21 +2,26 @@ import React from 'react';
 import Card from './Card.js';
 
 const	CardList	=	({ robots })	=>	{
-	const cardArray	=	robots.map((user, i) => {
-		return (
-			<Card 
-				key		=	{i} 
-				id		=	{robots[i].id} 
-				name	=	{robots[i].name} 
-				email	=	{robots[i].email}
-				phone	=	{robots[i].phone}
-			/>);
-	})
-	return	(
+	if (false) {
+		throw	new	Error	('Noooo...!');
+	}
+	return (
 		<div>
-			{ cardArray }
+		{
+			robots.map((user, i) => {
+				return (
+					<Card 
+						key={i} 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email} 
+						phone={robots[i].phone}
+					/>
+				);
+			})
+		}
 		</div>
-		);
+	);
 }
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import	CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
+import ErrorBoundry from '../components/ErrorBoundry';
 // import { robots } from './robots.js';
 import	Scroll from '../components/Scroll';
 import './App.css';
@@ -36,7 +37,9 @@ class App extends Component {
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
 				<Scroll>
-					<CardList robots={filteredRobots}/>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 		);
