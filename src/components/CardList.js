dba6d67bfdba59b5308fb2929ee0fe34d8bc230b
Fernando Zuniga <fernando.zunigag@gmail.com>
2018-12-04 10:54:27 -0600

    adding redux

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import Card from './Card.js';
+import Card from './Card';
 
-const	CardList	=	({ robots })	=>	{
-	if (false) {
-		throw	new	Error	('Noooo...!');
-	}
-	return (
-		<div>
-		{
-			robots.map((user, i) => {
-				return (
-					<Card 
-						key={i} 
-						id={robots[i].id} 
-						name={robots[i].name} 
-						email={robots[i].email} 
-						phone={robots[i].phone}
-					/>
-				);
-			})
-		}
-		</div>
-	);
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
 }
 
 export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import	{ Provider } from 'react-redux';
-import	{ createStore } from 'redux';
-import './index.css';
-import App from './containers/App.js';
-import * as serviceWorker from './serviceWorker';
-import	{ searchRobots } from './reducers';
-import	'tachyons';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
 
-const store = createStore(searchRobots)
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
+
+import './index.css';
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
